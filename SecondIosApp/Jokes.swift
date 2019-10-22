36079416ba778650c1f046a9fc0b0d5609f31aae
Cameron Casiano <CasianoC@chc.edu>
2019-10-22 10:51:02 -0400

    Added Joke logic

---
+++ SecondIosApp/Jokes.swift
@@ -0,0 +1,23 @@
+//
+//  Jokes.swift
+//  SecondIosApp
+//
+//  Created by Casiano, Cameron Joseph on 10/22/19.
+//  Copyright © 2019 Casiano, Cameron Joseph. All rights reserved.
+//
+
+import Foundation
+
+class Jokes {
+    
+    // Created a property that is an array of Strings
+    var jokes = [String] ()
+ 
+    // Created an init method, the equivalent of a java constructor
+    init() {
+        // includes 2 strings in the Jokes array
+        jokes.append("What do you call a bear with no teeth? A gummy bear!")
+        jokes.append("If con is the opposite of pro, it must mean Congress is the opposite of Progress!")
+    }
+}
+
--- SecondIosApp/ViewController.swift
+++ SecondIosApp/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+// This class is the equivalent of the MAin Activty java class in Adnroid Studio
 class ViewController: UIViewController {
 
+    // equivalent of the onCreate method in the java andorid class
+    // called whenthe scene that it controls is accessed (do initializtion work here)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // creating an instance of a joke
+        let joke = Jokes()
+        
+        //iterating through the jokes array
+        for joke in joke.jokes {
+            //printing out the jokes from the jokes array
+            print(joke)
+        }
     }
 
 
